5f3e46a4dc5628d28c33ef7df35dff192769fdf4
Sirio4ik <bulkinofaso@gmail.com>
2023-10-30 20:44:39 +0200

    FriendList, Transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import { Profile } from './profile/Profile';
 import { Statistics } from './statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import user from '../components/data/user.json';
 import data from '../components/data/data.json';
+import friends from '../components/data/friends.json';
+import transactions from '../components/data/transactions.json';
 
 export const App = () => {
   return (
@@ -14,6 +18,8 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
